c93c46559b80267f16f469b3dec36670a4c92a9e
aaronamk <aaronamk@gmail.com>
2018-12-08 23:56:45 -0500

    Added skeleton

---
+++ Engine/Engine.py
@@ -0,0 +1,16 @@
+import sys, time
+from PyQt5.QtGui import QPainter, QPen, QBrush, QColor
+from PyQt5.QtWidgets import QWidget, QApplication
+from PyQt5.QtCore import Qt, QRect, QPoint, QTimer
+
+class Engine(QWidget):
+    def __init__(self):
+        super.__init__()
+
+    def paintEvent(self, event):
+        pass
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Engine()
+    sys.exit(app.exec_())
