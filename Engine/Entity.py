685f4e72bf75efafa87422638c30ae8b8c46bc96
aaronamk <aaronamk@gmail.com>
2018-12-09 23:49:08 -0500

    Added variables to Entity Class

--- Engine/Entity.py
+++ Engine/Entity.py
@@ -1,4 +1,8 @@
 class Entity():
 
-    def __init__(self):
-        self.__parameters = [];
+    def __init__(self, image):
+        self.x
+        self.y
+        self.width
+        self.height
+        self.image = image # Implement animation (Array of images? Dealt with by qt?)
