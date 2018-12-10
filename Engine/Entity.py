52f4f565338dcd6330671de752189379e70c38e5
devets <devets@tuta.io>
2018-12-10 00:26:06 -0500

    Initialize variables to None in Entity.

--- Engine/Entity.py
+++ Engine/Entity.py
@@ -1,8 +1,9 @@
 class Entity():
 
     def __init__(self):
-        self.x
-        self.y
-        self.width
-        self.height
-        self.image # Implement animation (Array of images? Dealt with by qt?)
+        self.x = None
+        self.y = None
+        self.width = None
+        self.height = None
+        # Implement animation (Array of images? Dealt with by qt?)
+        self.image = None
