842dde4c0b755678138b622eedc0053ffd559fba
aaronamk <aaronamk@gmail.com>
2018-12-10 00:53:32 -0500

    Added collision detection to Entity.

--- Engine/Entity.py
+++ Engine/Entity.py
@@ -7,3 +7,9 @@ class Entity():
         self.height = None
         # Implement animation (Array of images? Dealt with by qt?)
         self.image = None
+
+    def is_touching(self, Entity):
+        if (self.x <= Entity.x + Entity.width and self.x + self.width <= Entity.x and
+        self.y <= self.x + Entity.height and self.y + self.height <= Entity.y):
+            return True
+        return False
