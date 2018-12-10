bed059173291cb427f52ed0ad94b62f6e22e41fb
devets <5199272+devets@users.noreply.github.com>
2018-12-10 02:23:27 -0500

    Added threaded run loop.

--- Engine/Engine.py
+++ Engine/Engine.py
@@ -1,6 +1,6 @@
 from World import World
 
-import sys, time
+import sys, time, threading
 from PyQt5.QtGui import QPainter, QPen, QBrush, QColor
 from PyQt5.QtWidgets import QWidget, QApplication
 from PyQt5.QtCore import Qt, QRect, QPoint, QTimer
@@ -10,6 +10,7 @@ class Engine(QWidget):
         super().__init__()
 
         self.__active_world = World(self)
+        self.__running = True
 
     def paintEvent(self, event):
         qp = QPainter()
@@ -20,7 +21,22 @@ class Engine(QWidget):
         
         qp.end()
 
+    def run(self):
+        while self.__running:
+            start_time = time.time()
+            
+            # Do stuff here
+            
+            if (time.time() - start_time < 1/60):
+                time.sleep(1/60 - (time.time() - start_time))
+
+
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = Engine()
+
+    t1 = threading.Thread(target=ex.run())
+    t1.start()
+
     sys.exit(app.exec_())
