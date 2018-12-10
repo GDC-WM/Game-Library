207dc1037cd8942d339f14ac5e67ff32e97695bb
devets <devets@tuta.io>
2018-12-09 23:18:03 -0500

    Added some basic, unfinished code for drawing in World.

--- Engine/World.py
+++ Engine/World.py
@@ -3,6 +3,15 @@ class World():
     def __init__(self):
         self.__entity_list = []
 
+        # View offset for the camera to facilitate scrolling
+        # These are added to the x and y value of each entity when rendered
+        self.__view_x = 0
+        self.__view_y = 0
+
+    def draw_screen(self, qp):
+        # TODO: fill this in with code to draw all the objects onscreen
+        pass
+
     def addEntity(self, entity):
         self.__entity_list.append(entity)
 
