871873ce54ad4a6622c9108fd486ec45b27aff9c
devets <5199272+devets@users.noreply.github.com>
2018-12-10 23:50:21 -0500

    Added basic physics code.

--- Engine/ActiveEntity.py
+++ Engine/ActiveEntity.py
@@ -4,10 +4,18 @@ class ActiveEntity(Entity):
 
     def __init__(self):
         super().__init__()
-        self.mass = 0;
-
+        self.physical = False
+        self.mass = 0
+        self.x_speed = 0
+        self.y_speed = 0
+    
     def physics(self):
-        pass
+        
+        if self.physical:
+            pass
+
+        self.x = self.x + self.x_speed
+        self.y = self.y + self.y_speed
 
     def run(self):
         pass
