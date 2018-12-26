8f24f1ed9595247c1c272e6aabfd9b7b2e65ab32
aaronamk <aaronamk@gmail.com>
2018-12-26 02:20:36 -0500

    Put ActiveEntity class into Entity file.

--- Engine/Entity.py
+++ Engine/Entity.py
@@ -37,3 +37,32 @@ class Entity():
             return None
 
         return self.image.scaled(int(100*scale), int(100*scale))
+
+class ActiveEntity(Entity):
+    """A type of entity that allows for movement within the world."""
+
+    def __init__(self):
+        super().__init__()
+        self.physical = False
+        self.mass = 0
+        self.x_speed = 0
+        self.y_speed = 0
+    
+    def physics(self):
+        """Applies the appropriate affects of game physics to the entity"""
+        if self.physical:
+            pass
+
+        self.x = self.x + self.x_speed
+        self.y = self.y + self.y_speed
+
+    def run(self):
+        """User implementation of run method."""
+        pass
+    
+    def push(self, vector):
+        """Adds a momentum vector to the entity\n
+        vector -- magnitude and direction (radians) in a tuple
+        """
+        self.x_speed = (self.mass*self.x_speed+vector[0]*math.cos(vector[1]))/self.mass
+        self.y_speed = (self.mass*self.y_speed+vector[0]*math.sin(vector[1]))/self.mass
