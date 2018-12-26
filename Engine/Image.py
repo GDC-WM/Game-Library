acbe47216d7065a588749090c1a7e8e69cb790a0
aaronamk <aaronamk@gmail.com>
2018-12-26 15:06:52 -0500

    Added ImageGroup class.

--- Engine/Image.py
+++ Engine/Image.py
@@ -4,9 +4,11 @@ from PyQt5.QtCore import Qt, QRect, QPoint
 
 class Image():
 
-    def __init__(self, image):
+    def __init__(self, filename):
+        self.__image = QImage(filename)
 
-        self.__image = QImage(image) 
+    def getImage(self):
+        return self.__image
 
     def setScale(self, xFactor, yFactor):
         self.__image = self.__image.scaled(self.getWidth()*xFactor, self.getHeight()*yFactor)
@@ -19,3 +21,21 @@ class Image():
 
     def getHeight(self):
         return self.__image.size().height()
+
+class ImageGroup():
+
+    def __init__(self):
+        self.__images = []
+        self.__image_index = 0
+
+    def addImage(self, image):
+        self.__images.append(image)
+    
+    def getImage(self):
+        return self.__images[self.__image_index]
+
+    def setStart(self):
+        self.image = self.__images[0]
+
+    def nextImage(self):
+        self.__image_index = (self.__image_index + 1) % len(self.__images)
